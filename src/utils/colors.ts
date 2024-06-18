360a4768e70c8e9491b59410ea68314950df33c0
HuseinHQ <mhusein463@gmail.com>
2024-06-18 16:39:31 +0700

    Initialize folder

---
+++ src/utils/colors.ts
@@ -0,0 +1,22 @@
+const colors = {
+  white: {
+    primary: '#FFFFFF',
+  },
+  red: {
+    primary: '#CE1212',
+  },
+  green: {
+    primary: '#0DBF3F',
+    darker: '#60884D',
+  },
+  grey: {
+    primary: '#ECEAEA',
+    lighter: '#F4F4F4',
+    darker: '#D9D9D9',
+  },
+  blue: {
+    primary: '#1A47BA',
+  },
+};
+
+export default colors;
