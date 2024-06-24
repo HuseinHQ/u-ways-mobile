b77631d1047eb4dd5cdb28c7c5abfea7f8c7e39b
HuseinHQ <mhusein463@gmail.com>
2024-06-24 16:35:52 +0700

    Add myhealthscreen (uncomplete)

--- src/screens/MyHealthScreen/index.tsx
+++ src/screens/MyHealthScreen/index.tsx
@@ -1,7 +1,16 @@
+import Colors from '@/utils/Colors';
 import React from 'react';
+import {SafeAreaView, StatusBar} from 'react-native';
 
 function MyHealthScreen(): React.JSX.Element {
-  return <></>;
+  return (
+    <SafeAreaView>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor={Colors.white.default}
+      />
+    </SafeAreaView>
+  );
 }
 
 export default MyHealthScreen;
