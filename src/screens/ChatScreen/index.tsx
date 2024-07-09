9939a3f1d2432688a519b2ad2e1752fbbd7950d9
HuseinHQ <mhusein463@gmail.com>
2024-07-09 16:36:20 +0700

    update

--- src/screens/ChatScreen/index.tsx
+++ src/screens/ChatScreen/index.tsx
@@ -1,7 +1,29 @@
+import Colors from '@/utils/Colors';
 import React from 'react';
+import {SafeAreaView, ScrollView, StatusBar, StyleSheet} from 'react-native';
+import Header from '../ChatScreen/LocalComponent/Header';
 
 function ChatScreen(): React.JSX.Element {
-  return <></>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar
+        backgroundColor={Colors.white.default}
+        barStyle="dark-content"
+      />
+
+      <ScrollView>
+        <Header title="Chat" withBackButton />
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.white.default,
+    paddingHorizontal: 20,
+    paddingTop: 10,
+  },
+});
 
 export default ChatScreen;
